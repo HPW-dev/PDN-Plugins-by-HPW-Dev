1590b3f43a2c55bdf85085dd0c82f9036635b2b1
Ilya Halilov <hpwdev0@gmail.com>
2025-07-22 00:08:05 +0300

    пред/пост гамма-кор

--- plugin/highp-threshold/highp-threshold.cs
+++ plugin/highp-threshold/highp-threshold.cs
@@ -9,16 +9,18 @@
 // Help: hpwdev0@gmail.com
 // Force Aliased Selection
 #region UICode
-ListBoxControl g_Desaturation_t = 0; // режим обесцвечивания|bt.709|0.35 0.5 0.15|bt.601|bt.2001|average|min|MinMax|max|red only|green only|blue only|Euclide
+ListBoxControl desaturation_mode = 0; // Desaturation mode|BT.709|0.35 0.5 0.15|BT.601|BT.2001|Average|Min|MinMax|Max|Red only|Green only|Blue only|Euclide
+ListBoxControl pre_gamma_mode = 0; // Gamma pre-processing|None|To 2.2|From 2.2
 CheckboxControl pre_inversion_val = false; // Pre-inversion
 DoubleSliderControl gamma_val = 1; // [0.001,2.4] Gamma
 DoubleSliderControl brightness_val = 0; // [-1,1] Brightness
 DoubleSliderControl contrast_val = 1; // [0,3] Contrast
 CheckboxControl post_inversion_val = false; // Post-inversion
+ListBoxControl post_gamma_mode = 0; // Gamma post-processing|None|To 2.2|From 2.2
 #endregion
 
 // режимы вычисления яркости
-enum Desaturation_t {
+enum desaturation_mode_t {
   bt709 = 0,
   R035_G05_B015,
   bt601,
@@ -33,6 +35,35 @@ enum Desaturation_t {
   euclide,
 }
 
+// режимы обработки гаммы
+enum gamma_mode_t {
+  none,
+  to_2_2,
+  from_2_2,
+}
+
+unsafe double gamma_to_2_2(double src) {
+  if (src <= 0.04045)
+    return src / 12.92;
+  return Math.Pow((src + 0.055) / 1.055, 2.4);
+}
+
+unsafe double gamma_from_2_2(double src) {
+  if (src <= 0.0031308)
+    return src * 12.92;
+  return 1.055 * Math.Pow(src, 1.0 / 2.4) - 0.055;
+}
+
+unsafe double gamma_processing(double src, int gamma_mode) {
+  switch ((gamma_mode_t)gamma_mode) {
+    default:
+    case gamma_mode_t.none: return src;
+    case gamma_mode_t.to_2_2: return gamma_to_2_2(src);
+    case gamma_mode_t.from_2_2: return gamma_from_2_2(src);
+  }
+  return src;
+}
+
 // конвертирует в серые оттенки
 unsafe double desaturate(ColorBgra src) {
   double b = src.B / 255.0;
@@ -43,26 +74,29 @@ unsafe double desaturate(ColorBgra src) {
     g = 1.0 - g;
     r = 1.0 - r;
   }
+  b = gamma_processing(b, pre_gamma_mode);
+  g = gamma_processing(g, pre_gamma_mode);
+  r = gamma_processing(r, pre_gamma_mode);
   double ret;
 
-  switch ((Desaturation_t)g_Desaturation_t) {
-    case Desaturation_t.bt2001: { ret = r * 0.2627 + g * 0.6780 + b * 0.0593; break; }
+  switch ((desaturation_mode_t)desaturation_mode) {
+    case desaturation_mode_t.bt2001: { ret = r * 0.2627 + g * 0.6780 + b * 0.0593; break; }
     default:
-    case Desaturation_t.bt709: { ret = r * 0.2126 + g * 0.7152 + b * 0.0722; break; }
-    case Desaturation_t.bt601: { ret = r * 0.299 + g * 0.587 + b * 0.114; break; }
-    case Desaturation_t.R035_G05_B015: { ret = r * 0.35 + g * 0.5 + b * 0.15; break; }
-    case Desaturation_t.euclide: { ret = Math.Sqrt(Math.Pow(r, 2.0) + Math.Pow(g, 2.0) + Math.Pow(b, 2.0)); break; }
-    case Desaturation_t.min: {
+    case desaturation_mode_t.bt709: { ret = r * 0.2126 + g * 0.7152 + b * 0.0722; break; }
+    case desaturation_mode_t.bt601: { ret = r * 0.299 + g * 0.587 + b * 0.114; break; }
+    case desaturation_mode_t.R035_G05_B015: { ret = r * 0.35 + g * 0.5 + b * 0.15; break; }
+    case desaturation_mode_t.euclide: { ret = Math.Sqrt(Math.Pow(r, 2.0) + Math.Pow(g, 2.0) + Math.Pow(b, 2.0)); break; }
+    case desaturation_mode_t.min: {
       ret = Math.Max(r, g);
       ret = Math.Max(ret, b);
       break;
     }
-    case Desaturation_t.max: {
+    case desaturation_mode_t.max: {
       ret = Math.Min(r, g);
       ret = Math.Min(ret, b);
       break;
     }
-    case Desaturation_t.minmax: {
+    case desaturation_mode_t.minmax: {
       var A = Math.Max(r, g);
       A = Math.Max(A, b);
       var B = Math.Min(r, g);
@@ -70,10 +104,10 @@ unsafe double desaturate(ColorBgra src) {
       ret = (A + B) * 0.5;
       break;
     }
-    case Desaturation_t.average: { ret = (r + g + b) / 3.0; break; }
-    case Desaturation_t.only_red: { ret = r; break; }
-    case Desaturation_t.only_green: { ret = g; break; }
-    case Desaturation_t.only_blue: { ret = b; break; }
+    case desaturation_mode_t.average: { ret = (r + g + b) / 3.0; break; }
+    case desaturation_mode_t.only_red: { ret = r; break; }
+    case desaturation_mode_t.only_green: { ret = g; break; }
+    case desaturation_mode_t.only_blue: { ret = b; break; }
   }
 
   return ret;
@@ -92,6 +126,7 @@ unsafe ColorBgra gray_to_rgb(double luma, byte alpha) {
   var ret = new ColorBgra();
   if (post_inversion_val)
     luma = 1.0 - luma;
+  luma = gamma_processing(luma, post_gamma_mode);
   ret.B = (byte)(clamp(luma * 255.0, 0.0, 255.0));
   ret.G = (byte)(clamp(luma * 255.0, 0.0, 255.0));
   ret.R = (byte)(clamp(luma * 255.0, 0.0, 255.0));
